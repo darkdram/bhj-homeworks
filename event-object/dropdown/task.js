fb60495d05d6a3697e8863c7a45bbe49c8ef4023
Vladimir <bakzzzbunny@gmail.com>
2021-08-11 18:32:21 +0300

    fix dropdown ex

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -12,14 +12,25 @@ Array.from(dropdownAll).forEach((dropdown) => {
 
         // toggle visibility if next sibling is dropdown list
         if (sibling && sibling.classList.contains('dropdown__list')) {
-            // prevent reload page after click on link from list for testing multiple dropdowns
-            // Array.from(sibling.querySelectorAll('a')).forEach((link) => {
-            //     link.onclick = function (e) {
-            //         e.preventDefault();
-            //     }
-            // });
-
             sibling.classList.toggle('dropdown__list_active');
         }
     };
+
+    // prevent reload page after click on link from list for testing multiple dropdowns
+    Array.from(dropdown.querySelectorAll('.dropdown__list a')).forEach((link) => {
+        link.onclick = function (e) {
+            // prevent click event after click on the link
+            e.preventDefault();
+            let dropdown_parent;
+            if (dropdown_parent = link.parentElement?.parentElement?.parentElement) { // if
+                let dropdown_value = dropdown_parent.querySelector('.dropdown__value')
+                if (dropdown_value) {
+                    dropdown_value.textContent = e.target.textContent.trim()
+                    // close dropdown after change value
+                    dropdown_value.dispatchEvent(new Event('click'))
+                    // dropdown_value.click() // dispatch click event
+                }
+            }
+        }
+    });
 });
