e1e4a4f8fd65833815945f802d2a2ec15838bd49
Vladimir <bakzzzbunny@gmail.com>
2021-09-13 22:02:11 +0300

    fix ex8

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,11 +1,21 @@
 const modal = document.querySelector('#subscribe-modal'),
     closeModal = document.querySelector('.modal__close');
 
+const getCookie = (name) => {
+    const value = "; " + document.cookie;
+    let parts=value.split("; " + name + "=");
+    if(parts.length === 2) {
+        return parts.pop().split(";").shift();
+    }
+};
+
 closeModal.addEventListener('click', () => {
-   sessionStorage.setItem('notOpenModal', 1);
+    document.cookie = 'notOpenModal=1';
     modal.classList.remove('modal_active');
 });
 
-if (sessionStorage.getItem('notOpenModal') != 1) {
+let notOpenModal = getCookie('notOpenModal');
+
+if (notOpenModal !== "1") {
     modal.classList.add('modal_active');
 }
